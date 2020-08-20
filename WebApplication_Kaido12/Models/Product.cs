38d648431520457dd435115374d03e60cba1fb01
海藤 一臣 <qaswr.cdwef.8642@icloud.com>
2020-08-20 15:24:01 +0900

    First Commit

---
+++ WebApplication_Kaido12/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Kaido12.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StandardPrice { get; set; }
+        public int ListPrice { get; set; }
+    }
+}
